f2ab71ad4d55a84770d51e0355842db19df421e8
Tim Perry <pimterry@gmail.com>
2025-12-10 17:16:24 +0100

    Outline of a server & project setup

---
+++ src/server.ts
@@ -0,0 +1,65 @@
+import * as http2 from 'http2';
+import * as httpolyglot from '@httptoolkit/httpolyglot';
+
+async function startAdminServer(options: {
+    adminPort: number;
+}) {
+    const adminServer = http2.createServer();
+    adminServer.on('session', (session) => {
+        session.on('stream', (stream, headers) => {
+            console.log('Received admin request:', headers[':method'], headers[':path']);
+            stream.respond({ ':status': 200 });
+            stream.end("Hello world");
+        });
+    });
+
+    await new Promise<void>((resolve) => {
+        adminServer.listen({ port: options.adminPort }, resolve);
+    });
+}
+
+async function startPublicUrlServer(options: {
+    publicPorts: number[];
+}) {
+    const server = httpolyglot.createServer({
+        socks: undefined,
+        unknownProtocol: undefined,
+        tls: undefined
+    }, (req, res) => {
+        console.log(`Received public url request: ${req.method} ${req.url}`);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello from Public URL Server!\n');
+    });
+
+    await Promise.all([
+        options.publicPorts.map((port) => {
+            return new Promise<void>((resolve) => {
+                server.listen(port, () => {
+                    console.log(`Public URL Server listening on port ${port}`);
+                    resolve();
+                });
+            });
+        })
+    ]);
+}
+
+export async function startServer(options: {
+    adminPort: number;
+    publicPorts: number[];
+}) {
+    await Promise.all([
+        startAdminServer({ adminPort: options.adminPort }),
+        startPublicUrlServer({ publicPorts: options.publicPorts })
+    ]);
+}
+
+// This is not a perfect test (various odd cases) but good enough
+const wasRunDirectly = import.meta.filename === process?.argv[1];
+if (wasRunDirectly) {
+    startServer({
+        adminPort: parseInt(process.env.ADMIN_PORT ?? '4000', 10),
+        publicPorts: (process.env.PUBLIC_PORTS ?? '8080').split(',').map(p => parseInt(p, 10))
+    }).then(() => {
+        console.log('Server started');
+    });
+}
